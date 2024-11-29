a863f604569c8fe83039edfc570f0bbcf124c2a5
vdmkkk <soloviov.vad@gmail.com>
2024-11-29 18:24:06 +0300

    update build dir

--- app/quasar.config.js
+++ app/quasar.config.js
@@ -40,6 +40,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
+      distDir: '/var/www/salut-frontend/SAIS-landing', // for linux only
       target: {
         browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node20',
